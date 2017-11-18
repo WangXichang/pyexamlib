4ecafe1a421d66f340886eda48ee6753c28e22e5
wangxichang <wangxichang@126.com>
2017-11-18 14:25:47 +0800

    modify report_df_describe with pearson relation

--- pyex_lib.py
+++ pyex_lib.py
@@ -1,5 +1,5 @@
-# -*- utf-8 -*-
 # version 2017-09-16
+# 2017-11-18
 
 import matplotlib.pyplot as plt
 # import matplotlib as mp
@@ -20,7 +20,7 @@ def exp_scoredf_normal(mean=70, std=10, maxscore=100, minscore=0, size=100000):
         DataFrame, columns = {'sf'}
     """
     df = pd.DataFrame({'sf': [max(minscore, min(int(np.random.randn(1)*std + mean), maxscore))
-                              for x in range(size)]})
+                              for _ in range(size)]})
     return df
 
 
@@ -77,8 +77,13 @@ def report_stats_describe(dataframe, decdigits=4):
 
     # for key, value in stats.describe(dataframe)._asdict().items():
     #    print(key, ':', value)
+    pr = [[float_str(stats.pearsonr(dataframe[x], dataframe[y])[0], 2, 4)
+           for x in dataframe.columns] for y in dataframe.columns]
     sd = stats.describe(dataframe)
     print('\trecords: ', sd.nobs)
+    print('\tpearson recorrelation:')
+    for i in range(len(dataframe.columns)):
+        print('\t', pr[i])
     print('\tmin: ', toround(sd.minmax[0], 0))
     print('\tmax: ', toround(sd.minmax[1], 0))
     print('\tmean: ', toround(sd.mean, decdigits))
@@ -87,5 +92,46 @@ def report_stats_describe(dataframe, decdigits=4):
     print('\tkurtosis: ', toround(sd.kurtosis, decdigits))
     dict = {'records': sd.nobs, 'max': sd.minmax[1], 'min': sd.minmax[0],
             'mean': sd.mean, 'variance': sd.variance, 'skewness': sd.skewness,
-            'kurtosis': sd.kurtosis}
+            'kurtosis': sd.kurtosis,
+            'relation':pr}
     return dict
+
+class ScoreStats():
+    """
+    :input
+        sdf:score dataframe
+    :result data
+        segdf: segment for some fields
+    :result fun
+        report_stats: stats result for sdf data, max,mean,min,skew,kurtosis
+        plot_line: plot distribute line graph
+        plot_scatter: plot scatter graph
+
+    """
+
+
+def pearson_relation(x, y):
+    plt.scatter(x, y)
+    return stats.pearsonr(x, y)[0]
+
+
+def float_str(x, d1, d2):
+    return f'%{d1}.{d2}f' % x
+
+
+def int_str(x, d):
+    return f'%{d}d' % x
+
+
+def df_format_digit2str(dfsource, intlen=2, declen=4, strlen=8):
+    df = dfsource[[dfsource.columns[0]]]
+    fdinfo = dfsource.dtypes
+    for fs in fdinfo.index:
+        if fdinfo[fs] in [np.float, np.float16, np.float32, np.float64]:
+            df[fs+'_f'] = dfsource[fs].apply(lambda x: float_str(x, intlen, declen))
+        elif fdinfo[fs] in [np.int, np.int8, np.int16, np.int32, np.int64]:
+            df[fs+'_f'] = dfsource[fs].apply(lambda x: int_str(x, 6))
+        elif fdinfo[fs] in [str]:
+            df[fs+'_f'] = dfsource[fs].apply(lambda x: x.rjust(strlen))
+    df.sort_index(axis=1)
+    return df
